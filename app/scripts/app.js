aa34aee9a1b118ffd5fb7bf2648ce1c90cfd30d1
Magnus Lööf <magnus.loof@basalt.se>
2016-09-18 17:19:28 +0200

    Add site generator
    - Initial commit
    - Created by Yeoman generator-angular

---
+++ app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name maglogithubioApp
+ * @description
+ * # maglogithubioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('maglogithubioApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name maglogithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the maglogithubioApp
+ */
+angular.module('maglogithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
